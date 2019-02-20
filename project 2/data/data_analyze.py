c5a0bb4c8dd76b8f4a4e991ec180ad5442e6cae6
bjason <leiyuchu@hotmail.com>
2019-02-20 15:50:10 -0800

    data

--- project 2/data/data_analyze.py
+++ project 2/data/data_analyze.py
@@ -7,91 +7,95 @@ import collections
 
 rankList = ['A+', 'A', 'A-', 'B+', '***', '**', '*', 'S', 'N', 'B', 'B-', 'C+', 'C',
             'C-', 'D+', 'D', 'D-', 'E+', 'E', 'E-', 'X']
-rankList = ['A+']
 ignorewords = set(['mr', 'mrs', 'one', 'two', 'said', 'i', '/i', '< i >', '< /i >', '<', '>', 'from',
                    'is', 'of', 'it', 'and', 'by', 'his', 'this', 'that', 'in', 'on', "i 'm", "i 'd", "i 've", "ca n't", "wo n't"])
 
 with open('rc.csv', 'w', newline='') as csvfile:
-    fieldnames = ['album', 'artist', 'polarity',
-                  'subjectivity', 'rank', 'word', 'frequency']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    fieldnames = ['album', 'artist', 'polarity', 'subjectivity']
+    writer = csv.writer(csvfile)
+    writer.writerow(f for f in fieldnames)
+
     wordcount_total = {}
 
     for rank in rankList:
-        with open(rank.replace('*', 'Star') + '.csv', encoding="utf-8") as csvfile:
-            fieldnames = ['isSoundTrack', 'rank', 'album',
-                          'artist', 'year', 'url', 'comment']
+        with open(rank.replace('*', 'Star') + '-modified.csv', 'w', newline='') as rankfile:
+            fieldnames = ['word', 'frequency']
+            rank_writer = csv.writer(rankfile)
+            rank_writer.writerow(f for f in fieldnames)
 
-            wordcount = {}
-            reviews = ""
+            with open(rank.replace('*', 'Star') + '.csv', encoding="ISO-8859-1") as csvfile:
+                fieldnames = ['isSoundTrack', 'rank', 'album',
+                              'artist', 'year', 'url', 'comment']
 
-            csv_reader = csv.reader(csvfile, delimiter=',')
-            for row in csv_reader:  # an album
-                print(row[2])
-                print(row[3])
+                wordcount = {}
+                # reviews = ""
 
-                d = {
-                    'album': row[2],
-                    'artist': row[3],
-                    'polarity': 0,
-                    'subjectivity': 0,
-                    'rank': '',
-                    'word': '',
-                    'frequency': 0
-                }
-                review = row[6].lower()
-                # .replace(".", "").replace(",", "").replace(":", "").replace(
-                #     "\"", "").replace("!", "").replace("â€œ", "").replace("â€˜", "").replace("*", "")
-                # reviews += review
-
-                sentence = TextBlob(review)
-
-                d['polarity'] = sentence.sentiment.polarity
-                d['subjectivity'] = sentence.sentiment.subjectivity
-
-                for word in sentence.noun_phrases:
-                    if word not in ignorewords:
-                        if word not in wordcount:
-                            wordcount[word] = 2
-                            wordcount_total[word] = 2
-                        else:
-                            wordcount[word] += 2
-                            wordcount_total[word] += 2
-
-                for tag in sentence.tags:
-                    if tag[1] != "JJ":
+                csv_reader = csv.reader(csvfile, delimiter=',')
+                row = 0
+                for row in csv_reader:  # an album
+                    if row == 0:
+                        row = 1
                         continue
 
-                    word = tag[0]
-                    if word not in ignorewords:
-                        if word not in wordcount:
-                            wordcount[word] = 1
-                            wordcount_total[word] = 1
-                        else:
-                            wordcount[word] += 1
-                            wordcount_total[word] += 1
+                    d = [row[2], row[3], 0, 0]
 
-                writer.writerow(d)
+                    review = row[6].lower()
+                    if len(review) == 0:
+                        continue
+                    # .replace(".", "").replace(",", "").replace(":", "").replace(
+                    #     "\"", "").replace("!", "").replace("â€œ", "").replace("â€˜", "").replace("*", "")
+                    # reviews += review
 
+                    sentence = TextBlob(review)
+
+                    d[2] = sentence.sentiment.polarity
+                    d[3] = sentence.sentiment.subjectivity
+
+                    for word in sentence.noun_phrases:
+                        if word not in ignorewords:
+                            if word not in wordcount:
+                                wordcount[word] = 2
+                                wordcount_total[word] = 2
+                            else:
+                                wordcount[word] += 2
+                                wordcount_total[word] += 2
+
+                    for tag in sentence.tags:
+                        if tag[1] != "JJ":
+                            continue
+
+                        word = tag[0]
+                        if word not in ignorewords:
+                            if word not in wordcount:
+                                wordcount[word] = 1
+                                wordcount_total[word] = 1
+                            else:
+                                wordcount[word] += 1
+                                wordcount_total[word] += 1
+
+                    writer.writerow(d)
+
+            n_print = 50
+            print("\nOK. The {} most common words are as follows\n".format(n_print))
+            word_counter = collections.Counter(wordcount)
+            for word, count in word_counter.most_common(n_print):
+                print(word, ": ", count)
+
+                d = [word, count]
+                rank_writer.writerow(d)
+
+    with open('total-modified.csv', 'w', newline='') as rankfile:
         n_print = 100
         print("\nOK. The {} most common words are as follows\n".format(n_print))
-        word_counter = collections.Counter(wordcount)
+        word_counter = collections.Counter(wordcount_total)
+
+        fieldnames = ['word', 'frequency']
+        rank_writer = csv.writer(rankfile)
+        rank_writer.writerow(f for f in fieldnames)
+
         for word, count in word_counter.most_common(n_print):
             print(word, ": ", count)
 
-            d = {
-                'album': '',
-                'artist': '',
-                'polarity': 0,
-                'subjectivity': 0,
-                'rank': rank,
-                'word': word,
-                'frequency': count
-            }
-            writer.writerow(d)
+            rank_writer.writerow([word, count])
 
-    n_print = 100
-    print("\nOK. The {} most common words are as follows\n".format(n_print))
-    word_counter = collections.Counter(wordcount_total)
-    for word, count in word_counter.most_common(n_print):
-            print(word, ": ", count)
+        # save for text cloud
---
+++ project 2/js/review.js
@@ -0,0 +1,90 @@
+var albumdata = [];
+var rankdata = [];
+var totalwords = [];
+
+function readData(data, error) {
+    if (error) console.log(error);
+
+    data.forEach(d => {
+        totalwords.push({
+            text: d.word,
+            size: d.frequency * d.frequency * 2 / 3
+        })
+    });
+
+    // var album = d.album;
+    // if (album.length == 0) {
+    //     var rank = d.rank;
+    //     var word = d.word;
+    //     var frequency = d.frequency;
+
+    //     rankdata.push({
+    //         rank: rank,
+    //         text: word,
+    //         size: frequency * 10
+    //     })
+    // } else {
+    //     var artist = d.artist;
+    //     var polarity = d.polarity;
+    //     var subjectivity = d.subjectivity;
+
+    //     albumdata.push({
+    //         album: album,
+    //         artist: artist,
+    //         polarity: polarity,
+    //         subjectivity: subjectivity
+    //     })
+    // }
+}
+
+function drawCloud() {
+    // console.log(totalwords);
+    let wid = window.innerWidth * 0.95;
+    let height = window.innerHeight;
+
+    var layout = d3.layout.cloud()
+        .size([wid, height])
+        .words(totalwords)
+        .padding(5)
+        .rotate(function () {
+            return ~~(Math.random() * 2) * 90;
+        })
+        .font("Impact")
+        .fontSize(function (d) {
+            return d.size;
+        })
+        .on("end", (words) => {
+            console.log(words);
+
+            d3.select("#vis").append("svg")
+                .attr("width", layout.size()[0])
+                .attr("height", layout.size()[1])
+                .append("g")
+                .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                .selectAll("text")
+                .data(words)
+                .enter().append("text")
+                .style("font-size", function (d) {
+                    return d.size + "px";
+                })
+                .style("font-family", "Impact")
+                .style("fill", "black")
+                .attr("text-anchor", "middle")
+                .attr("transform", function (d) {
+                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function (d) {
+                    return d.text;
+                });
+        });
+    layout.start();
+}
+
+function main() {
+    // draw cloud text
+    d3.csv("data/total-modified.csv")
+        .then(readData)
+        .then(drawCloud)
+}
+
+main()
